73815eeb32d5130d22c1aa53ec4592945b925898
Gonzalo <rodriguez.gzal@gmail.com>
2022-04-18 15:13:25 -0300

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Presentation from "./components/presentation";
 import Aboutme from "./components/aboutme";
 import Proyects from "./components/proyects";
 import Contactme from "./components/contactme";
-import Navigation from "./components/navigation";
 
 function App() {
   return (
